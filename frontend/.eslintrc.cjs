d0eafd17c382056761d3dead890f7ac0c342f1ce
Antoine Cheneau <antoine.cheneau@student-cs.fr>
2024-03-23 19:02:15 +0100

    Update ESLint configuration and package dependencies

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,11 +1,12 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2021: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/strict-type-checked',
     'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react/recommended',
+    'plugin:prettier/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
@@ -13,7 +14,10 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react', 'react-refresh'],
   rules: {
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react-refresh/only-export-components': ['error', { allowConstantExport: true }],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    '@eslint/semi': 'off',
+    'react/self-closing-comp': 'error',
   },
   parserOptions: {
     ecmaVersion: 'latest',
@@ -24,7 +28,6 @@ module.exports = {
       jsx: true,
     },
   },
-
   settings: {
     react: {
       version: 'detect',
