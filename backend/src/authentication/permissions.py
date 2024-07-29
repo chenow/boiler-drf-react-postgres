c5649002935610be277602a11dba3c23a7c69594
Antoine Cheneau <antoine.cheneau@student-cs.fr>
2024-07-29 18:14:30 +0200

    refactor: authentication permission
    make the permission support mypy

--- backend/src/authentication/permissions.py
+++ backend/src/authentication/permissions.py
@@ -1,9 +1,13 @@
 from rest_framework import permissions
 from rest_framework.request import Request
+from rest_framework.views import APIView
 
 
-class IsActiveAndAuthenticated(permissions.BasePermission):
+class IsActiveAndAuthenticated(permissions.BasePermission):  # type: ignore
     """Custom permission to only allow access to active and authenticated users."""
 
-    def has_permission(self, request: Request, view) -> bool:  # noqa: ANN001
+    def has_permission(self, request: Request, view: APIView) -> bool:
         return bool(request.user and request.user.is_authenticated and request.user.is_active)
+
+    def has_object_permission(self, request: Request, view: APIView, obj: object) -> bool:
+        return True
