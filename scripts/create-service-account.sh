a055727d06b7d6c7163865ac0f11f09e87ff556e
Antoine Cheneau <antoine.cheneau@student-cs.fr>
2024-07-30 11:54:10 +0200

    ci(pre-commit): setup git hooks using pre-commit to enforce code-quality

--- scripts/create-service-account.sh
+++ scripts/create-service-account.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Use this file to create a service account for github actions, and link an IAM policy to it.
-# You will then need to go to the IAM permissions tab in the console 
-# to give the required permissions to the service account: 
+# You will then need to go to the IAM permissions tab in the console
+# to give the required permissions to the service account:
 # - Artifact Registry Writer
 # - Service Account User
 # - Service Account Token Creator
@@ -10,8 +10,8 @@
 PROJECT_ID=boilder-react-drf-postges
 PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format='value(projectNumber)')
 
-gcloud iam service-accounts create githubactions \                                                              
-    --description="service acct for github actions" \                     
+gcloud iam service-accounts create githubactions \
+    --description="service acct for github actions" \
     --display-name="Github Actions"
 
 gcloud iam service-accounts add-iam-policy-binding githubactions@$PROJECT_ID.iam.gserviceaccount.com \
